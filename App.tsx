d05577b3285e5abd015f07ca5a566e3fc39177a6
Sriniketh <stponnada@gmail.com>
2025-09-25 23:09:59 +0530

    removing right sidebar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 // App.tsx
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
+// Import pages and components
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import ProfileSetup from './pages/ProfileSetup';
-import NotFound from './pages/NotFound';
+import NotFound from './pages/NotFound'; // <-- Import NotFound
 import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
 
@@ -16,18 +18,21 @@ const App = () => {
     <AuthProvider>
       <Router>
         <Routes>
+          {/* Public Route */}
           <Route path="/login" element={<Login />} />
 
+          {/* Protected Routes inside Layout */}
           <Route element={<ProtectedRoute />}>
             <Route element={<Layout />}>
               <Route path="/" element={<Home />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/setup" element={<ProfileSetup />} />
-              <Route path="/accounts/edit" element={<Settings />} /> {/* This is the settings page route */}
+              <Route path="/accounts/edit" element={<Settings />} />
             </Route>
           </Route>
-          
-          <Route path="*" element={<NotFound />} />
+
+          {/* Catch-all 404 Route */}
+          <Route path="*" element={<NotFound />} /> 
         </Routes>
       </Router>
     </AuthProvider>
