c7c51f22ec3fac6ebd5794bc5da60d3b7c5b5955
Sriniketh <stponnada@gmail.com>
2025-10-03 14:29:13 +0530

    root path

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Buffer } from 'buffer';
-import './index.css';
+import '../index.css';
 
 window.Buffer = Buffer;
 
